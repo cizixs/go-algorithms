aa145ea614d6db4e88b3ed251d589146ec7cedca
cizixs <wuwei4455@gmail.com>
2016-07-23 11:36:31 +0800

    Complete binary search tree.

--- binarysearchtree/btree.go
+++ binarysearchtree/btree.go
@@ -1,12 +1,18 @@
 package binarysearchtree
 
+import (
+	"errors"
+)
+
 // TODO: add support for any type, not just int
 type Node struct {
 	value       int   // Value the node stores
 	left, right *Node // pointers to the left and right children of the key
 }
 
+// Insert will add a new node to the tree whose root is Node
 func (n *Node) Insert(newNode *Node) {
+	// New node goes to the right part
 	if n.value < newNode.value {
 		if n.right == nil {
 			n.right = newNode
@@ -15,6 +21,7 @@ func (n *Node) Insert(newNode *Node) {
 		}
 	}
 
+	// New node goes to the left part
 	if n.value > newNode.value {
 		if n.left == nil {
 			n.left = newNode
@@ -24,6 +31,55 @@ func (n *Node) Insert(newNode *Node) {
 	}
 }
 
+func (n *Node) FindMin() (int, error) {
+	if n == nil {
+		return 0, errors.New("Empty tree has no Min element.")
+	}
+	for n.left != nil {
+		n = n.left
+	}
+	return n.value, nil
+}
+
+func (n *Node) FindMax() (int, error) {
+	if n == nil {
+		return 0, errors.New("Empty tree has no Max element.")
+	}
+	for n.right != nil {
+		n = n.right
+	}
+	return n.value, nil
+}
+
+func (n *Node) Delete(element int) (*Node, bool) {
+	deleted := false
+
+	if n == nil {
+		return n, false
+	}
+
+	if n.value < element {
+		n.right, deleted = n.right.Delete(element)
+	} else if n.value > element {
+		n.left, deleted = n.left.Delete(element)
+	} else if n.left != nil && n.right != nil {
+		candidate, _ := n.right.FindMin()
+		n.value = candidate
+		n.right, deleted = n.right.Delete(candidate)
+		deleted = true
+	} else {
+		if n.left == nil {
+			n = n.right
+		} else {
+			n = n.left
+		}
+		deleted = true
+	}
+	return n, deleted
+}
+
+// Walk function calls `f` on every node value
+// FIXME: Which is a better as parameter of `f`, `Node` or `node.value`?
 func (n *Node) Walk(f func(int)) {
 	if n == nil {
 		return
@@ -34,21 +90,25 @@ func (n *Node) Walk(f func(int)) {
 	n.right.Walk(f)
 }
 
+// BinarySearchTree store a root and the tree node numbers.
 type BinarySearchTree struct {
-	root      *Node
-	nodes_num int // nodes number: how many nodes are in the tree
+	root      *Node // pointer to root of the tree
+	nodes_num int   // nodes number: how many nodes are in the tree
 }
 
+// Return an initial new tree
 func New() *BinarySearchTree {
 	return &BinarySearchTree{}
 }
 
+// Return how many nodes are in the tree
 func (b *BinarySearchTree) Nodes() int {
 	return b.nodes_num
 }
 
-func (b *BinarySearchTree) Insert(value int) {
-	node := Node{value: value}
+// Insert an element into the tree
+func (b *BinarySearchTree) Insert(element int) {
+	node := Node{value: element}
 
 	if b.root == nil {
 		b.root = &node
@@ -58,11 +118,13 @@ func (b *BinarySearchTree) Insert(value int) {
 	b.nodes_num += 1
 }
 
+// Calls `f` on every node
 func (b *BinarySearchTree) Walk(f func(int)) {
 	b.root.Walk(f)
 }
 
-func (b *BinarySearchTree) Exists(element int) bool {
+// Check if an element exists in a tree
+func (b *BinarySearchTree) Contains(element int) bool {
 	n := b.root
 	for n != nil {
 		if n.value == element {
@@ -79,6 +141,50 @@ func (b *BinarySearchTree) Exists(element int) bool {
 	return false
 }
 
+// Return if the tree is an empty tree
 func (b *BinarySearchTree) IsEmpty() bool {
 	return b.nodes_num == 0
+}
+
+// Find and return a pointer to the node whose value equals to `element`
+func (b *BinarySearchTree) Find(element int) *Node {
+	n := b.root
+
+	for n != nil {
+		if n.value == element {
+			return n
+		}
+
+		if n.value < element {
+			n = n.right
+		} else {
+			n = n.left
+		}
+	}
+
+	return nil
+}
+
+// Find and return the smallest element in tree
+func (b *BinarySearchTree) FindMin() (int, error) {
+	return b.root.FindMin()
+}
+
+// Find and return the biggest element in tree
+func (b *BinarySearchTree) FindMax() (int, error) {
+	return b.root.FindMax()
+}
+
+// Delete the first appearence of the element
+// There are several cases:
+// - Node is a leaf, simply delete it
+// - Node has one child, point parent to the child, and delete the node
+// - Node has two children, replace the leftmost node of the right part with current node,
+//   then delete the leftmost node
+func (b *BinarySearchTree) Delete(element int) {
+	n, deleted := b.root.Delete(element)
+	b.root = n
+	if deleted {
+		b.nodes_num--
+	}
 }
--- binarysearchtree/btree_test.go
+++ binarysearchtree/btree_test.go
@@ -28,6 +28,88 @@ func TestInsert(t *testing.T) {
 	assert.Equal(b.Nodes(), len(data), "Counting tree nodes, expected %d, got %d", len(data), b.Nodes())
 
 	for _, element := range data {
-		assert.True(b.Exists(element), "Element %d should be in tree.", element)
+		assert.True(b.Contains(element), "Element %d should be in tree.", element)
 	}
+}
+
+func TestMin(t *testing.T) {
+	assert := assert.New(t)
+
+	b := binarysearchtree.New()
+
+	_, err := b.FindMin()
+	assert.Error(err, "Find min value in empty tree should return error")
+
+	data := []int{56, 37, 89, 2, 10, 72}
+	for _, element := range data {
+		b.Insert(element)
+	}
+	min, _ := b.FindMin()
+	assert.Equal(2, min)
+}
+
+func TestMax(t *testing.T) {
+	assert := assert.New(t)
+
+	b := binarysearchtree.New()
+
+	_, err := b.FindMax()
+	assert.Error(err, "Find max value in empty tree should return error")
+
+	data := []int{56, 37, 89, 2, 10, 72}
+	for _, element := range data {
+		b.Insert(element)
+	}
+	max, _ := b.FindMax()
+	assert.Equal(89, max)
+}
+
+func TestDelete(t *testing.T) {
+	assert := assert.New(t)
+	b := binarysearchtree.New()
+
+	data := []int{56, 37, 89, 2, 10, 72}
+	for _, element := range data {
+		b.Insert(element)
+	}
+
+	// Delete a one-child node
+	b.Delete(37)
+	assert.Equal(b.Nodes(), 5, "After deleting, tree should have 5 nodes, got %d", b.Nodes())
+	assert.False(b.Contains(37), "Delete 37 does not work.")
+
+	// Delete a not-exist node
+	b.Delete(100)
+	assert.False(b.Contains(100), "Delete a node not exist should do nothing")
+	assert.Equal(b.Nodes(), 5, "Delete a node not exist should not change tree's size")
+
+	// Delete a lead node
+	b.Delete(72)
+	assert.Equal(b.Nodes(), 4, "After deleting, tree should have 5 nodes, got %d", b.Nodes())
+	assert.False(b.Contains(72), "Delete 37 does not work.")
+}
+
+func TestDeleteOneNodeTree(t *testing.T) {
+	assert := assert.New(t)
+	b := binarysearchtree.New()
+	b.Insert(34)
+	b.Delete(34)
+
+	assert.Equal(b.Nodes(), 0, "Tree should have 0 node, got %d", b.Nodes())
+	assert.False(b.Contains(34), "Tree should not contain deleted node 34.")
+	assert.True(b.IsEmpty(), "Tree should be empty.")
+}
+
+func TestDeleteTwoChildNode(t *testing.T) {
+	assert := assert.New(t)
+	b := binarysearchtree.New()
+
+	data := []int{56, 37, 89, 2, 10, 72, 40}
+	for _, element := range data {
+		b.Insert(element)
+	}
+
+	b.Delete(40)
+	assert.Equal(b.Nodes(), 6, "After deleting, tree should have 5 nodes, got %d", b.Nodes())
+	assert.False(b.Contains(40), "Delete 37 does not work.")
 }
